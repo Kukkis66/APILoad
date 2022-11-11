7f4fb558dc9bcb34a2d6822b2eec96dcc5a7fef0
Kukkis66 <Kukkis66@github.com>
2022-11-11 11:30:13 +0200

    cleaning

--- core/views.py
+++ core/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import get_object_or_404, render, redirect
-from django.http import HttpResponse, Http404, FileResponse
-from django.views.generic import CreateView
-from django.urls import reverse_lazy
-from django.conf import settings
+from django.shortcuts import render, redirect
 
-from rest_framework.decorators import api_view, permission_classes, action
+
+
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import renderers
+
 
 from . serializers import FileSerializer
 from . forms import SignUpForm
